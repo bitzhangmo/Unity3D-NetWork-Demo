287a93c3e60c6e35e61bb4b293943fcbe436f349
王啸川 <m15048619182@163.com>
2019-05-09 11:38:54 +0800

    << Chapter 1

---
+++ Server Demo/EchoSever/EchoSever/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace EchoSever
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Socket
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint iPEp = new IPEndPoint(ipAdr, 8888);
+            listenfd.Bind(iPEp);
+
+            // Listen
+            listenfd.Listen(0);
+            Console.WriteLine("[服务器]启动成功");
+            while(true)
+            {
+                // Accept
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("[服务器]Accept");
+                // Receive
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+                Console.WriteLine("[服务器接收]" + readStr);
+                // Send
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+
+        }
+    }
+}
